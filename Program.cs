cc0ee4676307de333e37925b93e3a3d037783fca
Rohithapatchava <60814260+Rohithapatchava@users.noreply.github.com>
2020-02-19 17:40:03 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumtype
+{
+    class Program
+    {
+        enum department
+        {
+            physics = 10;
+            chemistry = 9;
+            mathematics = 14;
+            biology = 18;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("department id of mathematics is {0}",(int)department.mathematics);
+            Console.ReadKey();
+       }
+    }
+}
